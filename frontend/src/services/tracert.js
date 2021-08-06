48473e59fd571407a2ed1d08fd54862822455b37
Wahyu Arzani <hyuarzani@gmail.com>
2021-08-07 01:47:16 +0700

    connect login & add toast

--- frontend/src/services/tracert.js
+++ frontend/src/services/tracert.js
@@ -1,9 +1,11 @@
+import { HOST_URL } from '../env'
+
 export default class{
   constructor(deps){
       const enableDevTools = window.__GRPCWEB_DEVTOOLS__ || (() => {})
   
       this.proto = deps.proto
-      this.client = new deps.proto.TracertClient('https://api.borobudur.rijalasepnugroho.com', null, null)
+      this.client = new deps.proto.TracertClient(HOST_URL, null, null)
 
       enableDevTools([
         this.client,
