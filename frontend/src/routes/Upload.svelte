b3b516b8c2adce322fb69d003e475a30789d47b1
Wahyu Arzani <hyuarzani@gmail.com>
2021-08-07 19:08:22 +0700

    upload UI

--- frontend/src/routes/Upload.svelte
+++ frontend/src/routes/Upload.svelte
@@ -1,14 +1,34 @@
 <script>
   import { Link } from 'svelte-routing'
   import { token } from '../stores/token.js'
-
+  import Upload from "../components/Upload.svelte";
+  import { Images } from '../helper/images'
   const logout = () => {
       localStorage.clear()
       token.set(localStorage.getItem('token'))
   }
 </script>
 
-<h1>upload</h1>
-<h2>
-  <Link to="/login" on:click={logout}>Logout</Link>
-</h2>
+<div class="flex flex-wrap w-full h-full">
+  <div class="flex w-full p-4 align-center">
+
+    <main class="max-w-full px-4 mx-auto my-24 sm:mt-12 sm:px-6 md:mt-16 lg:my-24 lg:px-8">
+      <div class="sm:text-center lg:text-left">
+        <a href="/" class="flex items-center mb-8">
+          <i class="mr-4 fas fa-arrow-left"></i>
+          <p class="text-base">Kembali ke halaman utama</p>
+        </a>
+        <img class="object-cover w-64 h-full mb-4" src={Images.logo_poltekkes} alt="">
+        <h1 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-3xl md:text-3xl">
+          <span class="block xl:inline">UPLOAD IJAZAH UNTUK DI LEGALISIR</span>
+        </h1>
+        <p class="mt-3 mb-4 text-base text-gray-500 sm:mt-5 sm:text-s sm:max-w-3xl sm:mx-auto md:mt-5 md:text-s lg:mx-0">
+          Silakan Upload file ijazah yang akan di legalisir
+        </p>
+        
+        <hr class="my-8 md:min-w-full" />
+        <Upload />
+      </div>
+    </main>
+  </div>
+</div>
