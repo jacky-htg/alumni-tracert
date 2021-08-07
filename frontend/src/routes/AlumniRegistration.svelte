6d3c397bce7b0f75377b7b708b44f949477da632
Rizky Anas Bukhori <nyunkk@Rizkys-MacBook-Pro.local>
2021-08-08 00:04:23 +0700

    adding placeholder jurusan

--- frontend/src/routes/AlumniRegistration.svelte
+++ frontend/src/routes/AlumniRegistration.svelte
@@ -124,6 +124,7 @@
               <div class="col-span-2">
                 <label for="jurusan" class="block text-sm font-medium text-gray-700">Jurusan/Prodi</label>
                 <select on:blur="{changeJurusan}" id="jurusan" name="jurusan" autocomplete="jurusan" class="block w-full px-4 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-m">
+                  <option value="" disabled selected>Pilih Jurusan</option>
                   <option>Jurusan Analisis Kesehatan/TLM</option>
                   <option>Jurusan Farmasi</option>
                   <option>Prodi D4 Gizi</option>
