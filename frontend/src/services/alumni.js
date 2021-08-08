2f6e442be72b370455dfb5e4b4f5418a9dc6d858
Wahyu Arzani <hyuarzani@gmail.com>
2021-08-08 19:03:05 +0700

    connect alumni detail

--- frontend/src/services/alumni.js
+++ frontend/src/services/alumni.js
@@ -1,13 +1,20 @@
 import Tracert from './tracert';
+import Cookies from 'js-cookie'
 
 export default class extends Tracert{
   constructor(proto, alumniregistrationInput) {
     super(proto)
     this.req = alumniregistrationInput 
   }
-  registration (){
-      return this.client.alumniRegistration(this.req, {}).then(alumni=>{
-          return alumni
-      })
+  registration() {
+    return this.client.alumniRegistration(this.req, {}).then(alumni=>{
+        return alumni
+    })
+  }
+  get() {
+    const token = Cookies.get('token')
+    return this.client.alumniGet(this.req, { token }).then(alumni=>{
+      return alumni
+    })
   }
 }
