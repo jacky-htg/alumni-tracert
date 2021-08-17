74067b434032bfea60af2d2d2cfd8eb5df48f8c2
Wahyu Arzani <hyuarzani@gmail.com>
2021-08-17 16:57:31 +0700

    try back using storage binding

--- frontend/src/ProtectedRoute.svelte
+++ frontend/src/ProtectedRoute.svelte
@@ -1,13 +1,15 @@
 <script>
   import { Route } from 'svelte-routing'
   import accessDenied from './routes/accessDenied.svelte'
-  import Cookies from 'js-cookie'
+  // import Cookies from 'js-cookie'
+  import { token } from './stores/token.js';
 
   export let path
   export let component
+  $: isAuthenticated = $token;
 </script>
 
-{#if Cookies.get('token')}
+{#if isAuthenticated}
   <Route path={path} component={component} />
 {:else}
   <Route path={path} component={accessDenied} />
