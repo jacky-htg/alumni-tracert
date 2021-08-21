eeca6201a7679af81c5690ff036cafa4c77c8603
Wahyu Arzani <hyuarzani@gmail.com>
2021-08-21 17:55:51 +0700

    add home & login page

--- mobile/src/pages/Home.js
+++ mobile/src/pages/Home.js
@@ -1,16 +1,103 @@
 import React from 'react';
-import {Text, View, Button} from 'react-native';
+import {
+  ScrollView,
+  View,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from 'react-native';
 import {PAGES} from '../routes';
+import {Button, Text, Divider} from 'react-native-elements';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const Home = ({navigation}) => {
+  const onPressLogin = () => {
+    navigation.navigate(PAGES.LOGIN.path);
+  };
   return (
-    <View>
-      <Text>HomePage</Text>
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate(PAGES.LOGIN.path)}
+    <ScrollView>
+      <Image
+        style={{height: 200, width: windowWidth}}
+        resizeMethod="resize"
+        resizeMode="cover"
+        source={require('../assets/img/poltekkes-medan.jpeg')}
       />
-    </View>
+      <View style={{paddingHorizontal: 24, paddingTop: 24}}>
+        <Text style={{fontSize: 30, fontWeight: 'bold'}}>
+          ANTER Poltekkes Medan
+        </Text>
+        <Text style={{marginVertical: 8}}>
+          Anda sudah mempunyai akun? Silahkan login untuk bisa menikmati
+          fasilitas aplikasi ANTER Poltekkes Medan
+        </Text>
+        <Button
+          title="Login"
+          buttonStyle={{
+            width: windowWidth / 2,
+            backgroundColor: '#047857',
+          }}
+          onPress={onPressLogin}
+        />
+        <Divider
+          orientation="horizontal"
+          width={1}
+          style={{marginVertical: 24}}
+        />
+        <Text style={{marginBottom: 8}}>
+          Bagi alumni Poltekkes Medan yang belum mengisi kuesioner Tracer Study
+          Poltekkes Medan, silahkan tekan tombol dibawah ini.
+        </Text>
+        <Button
+          title="Kuisioner alumni tracer"
+          buttonStyle={{
+            backgroundColor: '#047857',
+          }}
+        />
+        <Divider
+          orientation="horizontal"
+          width={1}
+          style={{marginVertical: 24}}
+        />
+        <Text style={{marginBottom: 8}}>
+          Kunjungi halaman informasi kami lainnya
+        </Text>
+        <Button
+          title="Career Development Center"
+          type="clear"
+          buttonStyle={{
+            justifyContent: 'flex-start',
+            paddingLeft: 0,
+          }}
+          titleStyle={{
+            color: '#047857',
+          }}
+        />
+        <Button
+          title="Sapa Almuni"
+          type="clear"
+          buttonStyle={{
+            justifyContent: 'flex-start',
+            paddingLeft: 0,
+          }}
+          titleStyle={{
+            color: '#047857',
+          }}
+        />
+        <Button
+          title="Tentang Kami"
+          type="clear"
+          buttonStyle={{
+            justifyContent: 'flex-start',
+            paddingLeft: 0,
+          }}
+          titleStyle={{
+            color: '#047857',
+          }}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  Image,
   View,
 } from 'react-native';
+import {Button, Text, Card, Input} from 'react-native-elements';
 
 const Home = () => {
+  const [isLoading, setLoading] = useState(false);
+  const onPressLogin = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  };
   return (
-    <View>
-      <Text>Login</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 24,
+      }}>
+      <Card containerStyle={{width: '100%'}}>
+        <Image
+          resizeMethod="resize"
+          resizeMode="cover"
+          source={require('../assets/img/logo-poltekkes.png')}
+          style={{marginBottom: 24}}
+        />
+        <Card.Title>Selamat datang, silahkan login</Card.Title>
+        <Input placeholder="Username" />
+
+        <Input placeholder="Password" secureTextEntry={true} />
+
+        <Button
+          title="Login"
+          buttonStyle={{
+            backgroundColor: '#047857',
+          }}
+          loading={isLoading}
+          onPress={onPressLogin}
+        />
+      </Card>
     </View>
   );
 };
