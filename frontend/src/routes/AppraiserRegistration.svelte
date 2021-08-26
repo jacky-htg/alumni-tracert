8de79e3262744d620ff1a53bdd6f9a6521a9de2c
Dian Gunawan <digoens@gmail.com>
2021-08-26 23:11:53 +0700

    info password di kuisioner dan appraiser

--- frontend/src/routes/AppraiserRegistration.svelte
+++ frontend/src/routes/AppraiserRegistration.svelte
@@ -204,6 +204,17 @@
         <p class="mt-3 mb-4 text-base text-gray-500 sm:mt-5 sm:text-s sm:max-w-3xl sm:mx-auto md:mt-5 md:text-s lg:mx-0">Dengan demikian, tracer study dapat ikut membantu mengatasi permasalahan kesenjangan kesempatan kerja dan upaya perbaikannya.  Bagi Poltekkes Kemenkes Medan, informasi mengenai kompetensi yang relevan bagi dunia kerja dapat membantu upaya perbaikan kurikulum dan sistem pembelajaran.  Di sisi lain, dunia industri dan dunia kerja dapat melihat ke dalam instistusi pendidikan tinggi melalui hasil tracer study, dan dengan demikian dapat menyiapkan diri dengan menyediakan pelatihan-pelatihan yang lebih relevan bagi sarjana pencari kerja baru.
         Selanjutnya kami mengharapkan saudara dapat mengisi kuisioner ini dengan jujur, agar didapatkan hasil yang maksimal. Terima kasih.
         </p>
+
+        <div class="px-4 py-3 mt-8 text-blue-900 border-t-4 border-blue-500 rounded-b shadow-sm bg-blue-50" role="alert">
+          <div class="flex">
+            <div class="py-1"><svg class="w-6 h-6 mr-4 text-blue-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+            <div>
+              <p class="font-bold">Informasi assword alumni</p>
+              <p class="my-2 text-m sm:max-w-2xl">Kami akan mengirimkan password kepada akun yang Anda daftarkan pada aplikasi ini setelah Anda selesai mengisi kuisioner tracer study Poltekkes Medan.</p>
+              <p class="my-2 text-m sm:max-w-2xl">Password tersebut bisa Anda gunakan untuk mengakses dan memanfaatkan fitur online kemahasiswaan kami.</p>
+            </div>
+          </div>
+        </div>
         
         <hr class="my-8 md:min-w-full" />
         
