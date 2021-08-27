3c1176144cbff71e2bcb46f026af64b2c69e1f22
Dian Gunawan <digoens@gmail.com>
2021-08-27 19:41:52 +0700

    ijazah fix overflow

--- frontend/src/routes/Ijazah.svelte
+++ frontend/src/routes/Ijazah.svelte
@@ -68,67 +68,64 @@
 		
 		<Sidebar active="tambah-ijazah" sideBarMenus={SIDEBAR_USER}/>
 
-		<main class="flex-auto w-full min-w-0 px-20 pt-12 lg:static lg:max-h-full lg:overflow-visible">
+		<main class="max-w-5xl px-4 mx-auto my-24 sm:mt-12 sm:px-6 md:mt-16 lg:my-24 lg:px-8">
 			
 			<h1 class="mb-12 text-4xl font-bold">Tambah Ijazah</h1>
 			<p class="w-3/4 mb-8 text-xl text-gray-500">Tambahkan informasi ijazah milik anda yang belum teregister di E-legalisir</p>
 
-			<div class="overflow-hidden">
+			<div class="grid grid-cols-4 gap-4 mb-8">
+				<div class="col-span-4">
+					<label for="name" class="block text-sm font-medium text-gray-700">NIM</label>
+					<input on:change="{onChange}" type="text" name="nim" id="name" autocomplete="given-name" class="block w-full px-4 py-2 mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-m">
+				</div>
 
-				<div class="grid grid-cols-4 gap-4 mb-8">
-					<div class="col-span-4">
-						<label for="name" class="block text-sm font-medium text-gray-700">NIM</label>
-						<input on:change="{onChange}" type="text" name="nim" id="name" autocomplete="given-name" class="block w-full px-4 py-2 mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-m">
-					</div>
+				<div class="col-span-2">
+					<label for="jurusan" class="block text-sm font-medium text-gray-700">Jurusan/Prodi</label>
+					<select on:blur="{onChange}" id="jurusan" name="jurusan" autocomplete="jurusan" class="block w-full px-4 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-m">
+						<option value="" disabled selected>Pilih Jurusan</option>
+						<option>Jurusan Analisis Kesehatan/TLM</option>
+						<option>Jurusan Farmasi</option>
+						<option>Prodi D4 Gizi</option>
+						<option>Prodi D3 Gizi</option>
+						<option>Prodi D4 Keperawatan</option>
+						<option>Prodi D3 Keperawatan Medan</option>
+						<option>Jurusan Kesehatan Gigi</option>
+						<option>Prodi D4 Sanitasi</option>
+						<option>Prodi D3 Sanitasi</option>
+						<option>Prodi D4 Kebidanan</option>
+						<option>Prodi D3 Kebidanan Medan</option>
+						<option>Prodi D3 Kebidanan P. Sidimpuan</option>
+						<option>Prodi D3 Kebidanan P. Siantar</option>
+						<option>Prodi D3 Kebidanan Tarutung</option>
+						<option>Prodi D3 Kebidanan Karo</option>
+						<option>Prodi D3 Keperawatan Gunung Sitoli</option>
+						<option>Prodi D3 Keperawatan Dairi</option>
+						<option>Prodi D3 Keperawatan Sibolga</option>
+						<option>Prodi Profesi Bidan</option>
+					</select>
+				</div>
 
-					<div class="col-span-2">
-						<label for="jurusan" class="block text-sm font-medium text-gray-700">Jurusan/Prodi</label>
-						<select on:blur="{onChange}" id="jurusan" name="jurusan" autocomplete="jurusan" class="block w-full px-4 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-m">
-							<option value="" disabled selected>Pilih Jurusan</option>
-							<option>Jurusan Analisis Kesehatan/TLM</option>
-							<option>Jurusan Farmasi</option>
-							<option>Prodi D4 Gizi</option>
-							<option>Prodi D3 Gizi</option>
-							<option>Prodi D4 Keperawatan</option>
-							<option>Prodi D3 Keperawatan Medan</option>
-							<option>Jurusan Kesehatan Gigi</option>
-							<option>Prodi D4 Sanitasi</option>
-							<option>Prodi D3 Sanitasi</option>
-							<option>Prodi D4 Kebidanan</option>
-							<option>Prodi D3 Kebidanan Medan</option>
-							<option>Prodi D3 Kebidanan P. Sidimpuan</option>
-							<option>Prodi D3 Kebidanan P. Siantar</option>
-							<option>Prodi D3 Kebidanan Tarutung</option>
-							<option>Prodi D3 Kebidanan Karo</option>
-							<option>Prodi D3 Keperawatan Gunung Sitoli</option>
-							<option>Prodi D3 Keperawatan Dairi</option>
-							<option>Prodi D3 Keperawatan Sibolga</option>
-							<option>Prodi Profesi Bidan</option>
-						</select>
-					</div>
+				<div class="col-span-1">
+					<label for="entry-year" class="block text-sm font-medium text-gray-700">Tahun masuk</label>
+					<Yearpicker on:yearSelected={changeEntryYear}/>
+				</div>
+				
+				<div class="col-span-1">
+					<label for="graduation-year" class="block text-sm font-medium text-gray-700">Tahun lulus</label>
+					<Yearpicker on:yearSelected={changeGraduationYear}/>
+				</div>
 
-					<div class="col-span-1">
-						<label for="entry-year" class="block text-sm font-medium text-gray-700">Tahun masuk</label>
-						<Yearpicker on:yearSelected={changeEntryYear}/>
-					</div>
+				<div class="col-span-4">
+					<label for="no-ijazah" class="block text-sm font-medium text-gray-700">Nomor Ijazah</label>
+					<input on:change="{onChange}" type="text" name="no-ijazah" id="no-ijazah" autocomplete="no-ijazah" class="block w-full px-4 py-2 mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-m">
+				</div>              
+						
+			</div>
 					
-					<div class="col-span-1">
-						<label for="graduation-year" class="block text-sm font-medium text-gray-700">Tahun lulus</label>
-						<Yearpicker on:yearSelected={changeGraduationYear}/>
-					</div>
-
-					<div class="col-span-4">
-						<label for="no-ijazah" class="block text-sm font-medium text-gray-700">Nomor Ijazah</label>
-						<input on:change="{onChange}" type="text" name="no-ijazah" id="no-ijazah" autocomplete="no-ijazah" class="block w-full px-4 py-2 mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-m">
-					</div>              
-              
-				</div>
-            
-				<div class="px-4 py-3 text-right bg-gray-50 sm:px-6">
-					<button on:click="{tambah}" class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-						Tambah
-					</button>
-				</div>
+			<div class="px-4 py-3 text-right bg-gray-50 sm:px-6">
+				<button on:click="{tambah}" class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+					Tambah
+				</button>
 			</div>
 			
 		</main>
