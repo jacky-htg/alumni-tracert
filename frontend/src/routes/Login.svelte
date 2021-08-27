c59103a40f24a436a5cd4cabc4d141211f64b649
Rizky Anas Bukhori <nyunkk@Rizkys-MacBook-Pro.local>
2021-08-27 22:11:44 +0700

    [FE] Fitur lupa password #23 WIP

--- frontend/src/routes/Login.svelte
+++ frontend/src/routes/Login.svelte
@@ -38,7 +38,8 @@
   // let password = 'hariINI@2021'
   let state = {
     username: '',
-    password: ''
+    password: '',
+    emptyUsername: false,
   }
   const handleInput = (e) => {
     const { value, name } = e.target
@@ -64,14 +65,46 @@
       notifications.danger(e.message)
     }
   };
+
+  const forgotPass = async () => {
+    if (state.username !== '') {
+      // try {
+        
+      // } catch(e) {
+      //   isLoading = false;
+      //   notifications.danger(e.message)
+      // }
+    } else {
+      state.emptyUsername = true;
+    }
+  }
   
 </script>
 <div class="container flex flex-col items-center justify-center w-screen h-screen mx-auto">
   <img class="object-cover w-64 mb-8" src={Images.logo_poltekkes} alt="">
   <p class="mb-6 text-xl text-black">Selamat datang, silahkan login</p>
   <form on:submit|preventDefault={login} class="flex flex-col w-full p-8 mt-10 bg-gray-100 rounded-lg lg:w-2/6 md:w-1/2 md:mt-0">
-      <Input label="Username" name="username" on:input={handleInput} bind:value={state.username} placeholder="username" type="text" required/>
-      <Input label="Password" name="password" on:input={handleInput} bind:value={state.password} placeholder="password" type="password" required/>
+      <Input
+        label="Username"
+        name="username"
+        on:input={handleInput}
+        bind:value={state.username}
+        placeholder="username"
+        type="text"
+        required
+        error={state.emptyUsername}
+      />
+      <Input
+        label="Password"
+        name="password"
+        on:input={handleInput}
+        bind:value={state.password}
+        placeholder="password"
+        type="password"
+        required
+      />
       <Button isLoading={isLoading}>Login</Button>
+
+      <p on:click="{forgotPass}" class="cursor-pointer text-center mt-6 text-md text-blue-600">Lupa Password ?</p>
   </form>
 </div>
